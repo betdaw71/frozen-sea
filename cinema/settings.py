8575f6c08665cad668865fb7bba08f3c217914b4
betdav71 <babayan.dawid2017@yandex.ru>
2019-05-04 15:48:23 +0300

    IframeFix4

--- cinema/settings.py
+++ cinema/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 X_FRAME_OPTIONS = 'DENY'
-X_FRAME_OPTIONS = 'ALLOW-FROM http://hdkino.vip/'
+X_FRAME_OPTIONS = 'ALLOW-FROM http://cdn.filmtybe.pl/'
 
 
 ROOT_URLCONF = 'cinema.urls'
