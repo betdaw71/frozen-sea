1aebfae6b3d8b5c93e38615b2e71ab65513b15e6
betdav71 <babayan.dawid2017@yandex.ru>
2019-05-05 17:45:46 +0300

    MedaiFix6

--- cinema/settings.py
+++ cinema/settings.py
@@ -136,23 +136,23 @@ import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
-AWS_ACCESS_KEY_ID = 'AKIAR5VRFDXAKFGUZ2OV'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'cinema.media'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+# AWS_ACCESS_KEY_ID = 'AKIAR5VRFDXAKFGUZ2OV'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_STORAGE_BUCKET_NAME = 'cinema.media'
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
 
-# AWS S3 Static
-AWS_STATIC_LOCATION = 'static'
-STATIC_URL = 'http://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'cinema.storage_backends.StaticStorage'
+# # AWS S3 Static
+# AWS_STATIC_LOCATION = 'static'
+# STATIC_URL = 'http://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
+# STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_STORAGE = 'cinema.storage_backends.StaticStorage'
 
-#AWS S3 Private Media Upload
-AWS_MEDIA_LOCATION = 'media'
-PRIVATE_FILE_STORAGE = 'cinema.storage_backends.MediaStorage'
+# #AWS S3 Private Media Upload
+# AWS_MEDIA_LOCATION = 'media'
+# PRIVATE_FILE_STORAGE = 'cinema.storage_backends.MediaStorage'
 
-#AWS S3 Public Media Upload
-AWS_PUBLIC_LOCATION = 'public'
-DEFAULT_FILE_STORAGE = 'cinema.storage_backends.PublicStorage'
+# #AWS S3 Public Media Upload
+# AWS_PUBLIC_LOCATION = 'public'
+# DEFAULT_FILE_STORAGE = 'cinema.storage_backends.PublicStorage'
