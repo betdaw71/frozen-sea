59e62a2c5527b6e1fe55cee841b27099ede95fe8
betdav71 <babayan.dawid2017@yandex.ru>
2019-05-05 18:55:11 +0300

    FixAdmin4

--- cinema/settings.py
+++ cinema/settings.py
@@ -156,6 +156,8 @@ DATABASES['default'].update(db_from_env)
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static') 
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/' 
 
