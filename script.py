7d64e1e287f2be7f468d3442c3a4dc8b866f527c
betdav71 <babayan.dawid2017@yandex.ru>
2019-05-07 22:13:25 +0300

    Fix

--- script.py
+++ script.py
@@ -22,7 +22,7 @@ with open('dict_output1.csv','r',newline='') as file:
 				leng = 0
 			cate = Category.objects.get(pk=1)
 			artic = Article.objects.create(title=row[0],original_name=row[1],
-				body=row[2],rating=row[3],iframe=row[4],img=n,category=cate,
+				body=row[2],rating=row[3],iframe=row[4],img=n,category=Category.objects.get(title='Serials'),
 				data=row[8],length=leng,actor=row[10],director=row[11],country=row[12],quolity=row[13],
 				translation=row[14],trailer=row[15])
 
